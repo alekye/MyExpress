dfc3ef984b0aadd21ea1be60066eadfe9dea0695
Alek Ye <alek.ye@smartaid.cn>
2020-05-18 10:37:23 +0800

    static file ok

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const Config = require("./Config");
+const conf = Config.load("default.json") || {};
+console.log("config = ", conf);
+const port = conf.port || 5000;
+
+const express = require("express");
+const app = express();
+
+// 静态文件服务
+const fileDir = conf.fileDir || "./wwwroot";
+const fullFilePath = path.join(process.mainModule.path, fileDir);
+console.log("web file dir = ", fullFilePath);
+// 静态文件配置
+const options = {
+  dotfiles: "ignore",
+  etag: false,
+  extensions: ["htm", "html"],
+  index: "index.html",
+  // maxAge: 0,
+  redirect: false,
+  setHeaders: function(res, path, stat) {
+    res.set("x-timestamp", Date.now());
+  }
+};
+app.use(express.static(fullFilePath, options));
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
